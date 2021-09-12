ad043627086debaae8cfcc26ef7b99edf1b6c45e
ayumi201604 <boayu.c201604@gmail.com>
2021-09-12 21:38:40 +0900

    バリデーション条件コード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+    validates :first_name
+    validates :last_name
   end
-  validates :first_name_kana, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :last_name_kana, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birth_day, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
