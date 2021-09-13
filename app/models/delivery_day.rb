4c7d94c6ca00a9428cd0c93c3bbd2cae9ada6e3d
ayumi201604 <boayu.c201604@gmail.com>
2021-09-13 22:10:54 +0900

    DB設計、itemsテーブルカラム名dayを変更

--- app/models/delivery_day.rb
+++ app/models/delivery_day.rb
@@ -1,4 +1,4 @@
-class Day < ActiveHash::Base
+class DeliveryDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '1〜2日で発送' },
@@ -6,5 +6,5 @@ class Day < ActiveHash::Base
     { id: 4, name: '4〜7日で発送' }
   ]
   include ActiveHash::Associations
-  has_many :item
+  has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,8 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :category
   belongs_to :condition
-  belongs_to :day
+  belongs_to :delivery_day
   belongs_to :shippingcost
   belongs_to :user
   has_one_attached :image
-  def was_attached?
-    self.image.attached?
-  end
 end
