81b8cb7466381a2c58693cd73a1151a638ca148a
ayumi201604 <boayu.c201604@gmail.com>
2021-09-13 21:52:01 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   validates :birth_day, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :item
+  has_many :items
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
